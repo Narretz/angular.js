aa2386b593aff583fca8403338779b71766fa720
Martin Staffa <mjstaffa@gmail.com>
2016-08-17 11:48:59 +0200

    fix(input[number]): validate the viewValue for min/max
    This makes it consistent with all other input types, whose viewValue
    is not modified via a parser.
    
    BREAKING CHANGE:
    
    Input[type=number] now validates the ngModel.$viewValue instead of
    the ngModel.$modelValue, i.e. the value that is visible to the user
    is validated. This makes it consistent with all other input types
    for which the $viewValue is not modified via a parser. It also
    makes more sense to the user, as the modelValue might be modified,
    and a validation error would not necessarily reflect what the user
    sees in the input.

--- src/ng/directive/input.js
+++ src/ng/directive/input.js
@@ -1506,8 +1506,8 @@ function numberInputType(scope, element, attr, ctrl, $sniffer, $browser) {
 
   if (isDefined(attr.min) || attr.ngMin) {
     var minVal;
-    ctrl.$validators.min = function(value) {
-      return ctrl.$isEmpty(value) || isUndefined(minVal) || value >= minVal;
+    ctrl.$validators.min = function(modelValue, viewValue) {
+      return ctrl.$isEmpty(viewValue) || isUndefined(minVal) || viewValue >= minVal;
     };
 
     attr.$observe('min', function(val) {
@@ -1522,8 +1522,8 @@ function numberInputType(scope, element, attr, ctrl, $sniffer, $browser) {
 
   if (isDefined(attr.max) || attr.ngMax) {
     var maxVal;
-    ctrl.$validators.max = function(value) {
-      return ctrl.$isEmpty(value) || isUndefined(maxVal) || value <= maxVal;
+    ctrl.$validators.max = function(modelValue, viewValue) {
+      return ctrl.$isEmpty(viewValue) || isUndefined(maxVal) || viewValue <= maxVal;
     };
 
     attr.$observe('max', function(val) {
